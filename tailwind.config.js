8b7ebcff5241df968afe64d0ca176a92c40e3b0d
tifani <tifanisds@gmail.com>
2025-05-20 13:15:56 -0300

    Adiciona o projeto completo

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+      },
+      boxShadow: {
+        'solid-green': '2px 2px 0 #58A700',
+        'solid-dark-blue': '2px 2px 0 #042C60',
+        'solid-gray': '2px 2px 0 #E5E5E5',
+      },
+    },
   },
   plugins: [],
 }
